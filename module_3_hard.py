79dd978ab874d778da11cb8497951ef9aa9ea51f
Юрий <urtets@gmail.com>
2024-09-10 11:19:04 +0300

    Hard_Task

---
+++ module_3_hard.py
@@ -0,0 +1,26 @@
+def calculate_structure_sum(data_structure):
+    summ = 0
+    if isinstance(data_structure, (int, float)):
+        summ += data_structure
+    elif isinstance(data_structure, str):
+        summ += len(data_structure)
+    elif isinstance(data_structure, dict):
+        for key, value in data_structure.items():
+            summ += calculate_structure_sum(key)
+            summ += calculate_structure_sum(value)
+    else:
+        for item in data_structure:
+            summ += calculate_structure_sum(item)
+    return summ
+
+
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+result = calculate_structure_sum(data_structure)
+print(result)
